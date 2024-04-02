c4377b22b04d8d1f8e7c819d17ba1fb7665b9e3d
artefarte001 <cheikhsane656@gmail.com>
2024-04-02 12:47:14 +0000

    sql dupmp

--- habitant.sql
+++ habitant.sql
@@ -1,22 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 02 avr. 2024 à 14:45
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
 
-DROP TABLE IF EXISTS `membre`;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `habitant`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `membre`
+--
 
 CREATE TABLE `membre` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `prenom` varchar(60) DEFAULT NULL,
   `nom` varchar(60) DEFAULT NULL,
-  `age` int DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
   `sexe` varchar(40) DEFAULT NULL,
   `stituation` varchar(60) DEFAULT NULL,
-  `statut` varchar(70) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `statut` varchar(70) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `membre`
+--
 
-LOCK TABLES `membre` WRITE;
+INSERT INTO `membre` (`id`, `prenom`, `nom`, `age`, `sexe`, `stituation`, `statut`) VALUES
+(1, 'ndeye', 'cisse', 12, 'feminin', 'celibataire', 'civile'),
+(2, 'cheikh tidiane', 'Sané', 17, 'masculin', 'celibataire', 'civile');
 
-INSERT INTO `membre` VALUES (1,'ndeye','cisse',12,'feminin','celibataire','civile');
-INSERT INTO `membre` VALUES (2,'cheikh tidiane','Sané',17,'masculin','celibataire','civile');
+--
+-- Index pour les tables déchargées
+--
 
+--
+-- Index pour la table `membre`
+--
+ALTER TABLE `membre`
+  ADD PRIMARY KEY (`id`);
 
-UNLOCK TABLES;
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `membre`
+--
+ALTER TABLE `membre`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
