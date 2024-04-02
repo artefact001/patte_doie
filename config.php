77ba6e1be085480861d96b30f211b27a68e700bf
artefarte001 <cheikhsane656@gmail.com>
2024-04-02 13:35:07 +0000

    ajout de la partie update

---
+++ config.php
@@ -0,0 +1,21 @@
+
+<?php
+include_once "etudiant.php";
+// création des constantes pour les informations de la base de données
+define("DB_SERVER", "localhost");
+define("USER_NAME", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "habitant");
+
+// connexion avec la base de données
+try {
+    $connexion = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, USER_NAME, DB_PASSWORD);
+    // Configuration de PDO pour afficher les erreurs SQL
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} 
+// affichage d'un message en cas d'erreur
+catch (PDOException $e) {
+    die("Erreur :: Impossible de se connecter à la base de données : " . $e->getMessage());
+}
+?>
